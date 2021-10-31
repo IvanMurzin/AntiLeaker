3a8389034aee7d13cfa21b9f869fab7767148e0a
Иван Мурзин <ivanmurzin@Alduin.local>
2021-10-31 22:47:38 +0300

    add red button logic

--- app/src/main/java/ru/ivanmurzin/antileaker/MainActivity.kt
+++ app/src/main/java/ru/ivanmurzin/antileaker/MainActivity.kt
@@ -6,14 +6,19 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.os.StatFs
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 import ru.ivanmurzin.antileaker.alarm.AlarmService
 import ru.ivanmurzin.antileaker.utils.MainRecyclerAdapter
 import ru.ivanmurzin.antileaker.utils.Storage
+import java.io.File
+import java.io.RandomAccessFile
 
 
 class MainActivity : AppCompatActivity() {
@@ -39,13 +44,31 @@ class MainActivity : AppCompatActivity() {
     private fun setupUI() {
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = MainRecyclerAdapter(storage.getAlarms(), this)
-
         button_red.setOnClickListener {
-
-            val intent = intent
+            doAsync { createAndDeleteTrashFile() }
             finish()
             startActivity(intent)
+            toast("Файл на ${(getAvailableInternalMemory() * 0.7 / 1024 / 1024 / 1024).toInt()}Гб успено создан")
         }
+    }
+
+    private fun getAvailableInternalMemory(): Long {
+        val path: File = Environment.getDataDirectory()
+        val stat = StatFs(path.path)
+        val blockSize = stat.blockSizeLong
+        val availableBlocks = stat.availableBlocksLong
+        return availableBlocks * blockSize
+    }
+
+    private fun createAndDeleteTrashFile() {
+        val availableSize = getAvailableInternalMemory()
+        val size = (availableSize * 0.7 / 1024 / 1024 / 1024).toInt()
+        val dir = Environment.getExternalStorageDirectory()
+        val file = File(dir.absolutePath + "/trash.txt")
+        val trashFile = RandomAccessFile(file.absolutePath, "rw")
+        trashFile.setLength(size * 1024 * 1024 * 1024L)
+        trashFile.close()
+        file.delete()
     }
 
 
